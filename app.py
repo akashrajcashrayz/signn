f504db18e6e1fe33ae772e5f633ced54d4e0f940
akashrajcashrayz <53985845+akashrajcashrayz@users.noreply.github.com>
2021-08-13 17:51:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,191 @@
+from sys import stdout
+from makeup_artist import Makeup_artist
+import logging
+from flask import Flask, render_template, Response
+from flask_socketio import SocketIO, emit
+from camera import Camera
+from utils import base64_to_pil_image, pil_image_to_base64
+import h5py
+import numpy as np
+import mediapipe as mp
+
+app = Flask(__name__)
+model = h5py.File('action.h5','r')
+actions = np.array(['hello', 'thanks', 'iloveyou'])
+label_map = {label:num for num, label in enumerate(actions)}
+# Thirty videos worth of data
+no_sequences = 30
+
+# Videos are going to be 30 frames in length
+sequence_length = 30
+mp_holistic = mp.solutions.holistic # Holistic model
+mp_drawing = mp.solutions.drawing_utils # Drawing utilities
+app.logger.addHandler(logging.StreamHandler(stdout))
+app.config['SECRET_KEY'] = 'secret!'
+app.config['DEBUG'] = True
+socketio = SocketIO(app)
+camera = Camera(Makeup_artist())
+
+
+@socketio.on('input image', namespace='/test')
+def test_message(input):
+    input = input.split(",")[1]
+    camera.enqueue_input(input)
+    image_data = input # Do your magical Image processing here!!
+    #image_data = image_data.decode("utf-8")
+    image_data = "data:image/jpeg;base64," + image_data
+    print("OUTPUT " + image_data)
+    emit('out-image-event', {'image_data': image_data}, namespace='/test')
+    #camera.enqueue_input(base64_to_pil_image(input))
+
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    app.logger.info("client connected")
+
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
+    image.flags.writeable = False                  # Image is no longer writeable
+    results = model.process(image)                 # Make prediction
+    image.flags.writeable = True                   # Image is now writeable 
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
+    return image, results
+def draw_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS) # Draw face connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw right hand connections
+
+def draw_styled_landmarks(image, results):
+    # Draw face connections
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS, 
+                             mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
+                             mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)
+                             ) 
+    # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                             mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
+                             mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
+                             ) 
+    # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                             mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4), 
+                             mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
+                             ) 
+    # Draw right hand connections  
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                             mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4), 
+                             mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+                             ) 
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
+    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(468*3)
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    return np.concatenate([pose, face, lh, rh])
+
+colors = [(245,117,16), (117,245,16), (16,117,245)]
+def prob_viz(res, actions, input_frame, colors):
+    output_frame = input_frame.copy()
+    for num, prob in enumerate(res):
+        cv2.rectangle(output_frame, (0,60+num*40), (int(prob*100), 90+num*40), colors[num], -1)
+        cv2.putText(output_frame, actions[num], (0, 85+num*40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255,255,255), 2, cv2.LINE_AA)
+        
+    return output_frame
+
+def gen():
+  sequence = []
+  sentence = []
+  threshold = 0.8
+
+  
+  # Set mediapipe model 
+  with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+      while cap.isOpened():
+
+          # Read feed
+          ret, frame = cap.read()
+
+          # Make detections
+          image, results = mediapipe_detection(frame, holistic)
+          print(results)
+          
+          # Draw landmarks
+          draw_styled_landmarks(image, results)
+          
+          # 2. Prediction logic
+          keypoints = extract_keypoints(results)
+  #         sequence.insert(0,keypoints)
+  #         sequence = sequence[:30]
+          sequence.append(keypoints)
+          sequence = sequence[-30:]
+          
+          if len(sequence) == 30:
+              res = model.predict(np.expand_dims(sequence, axis=0))[0]
+              print(actions[np.argmax(res)])
+              
+              
+          #3. Viz logic
+              if res[np.argmax(res)] > threshold: 
+                  if len(sentence) > 0: 
+                      if actions[np.argmax(res)] != sentence[-1]:
+                          sentence.append(actions[np.argmax(res)])
+                  else:
+                      sentence.append(actions[np.argmax(res)])
+
+              if len(sentence) > 5: 
+                  sentence = sentence[-5:]
+
+              # Viz probabilities
+              image = prob_viz(res, actions, image, colors)
+              
+          cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
+          cv2.putText(image, ' '.join(sentence), (3,30), 
+                        cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+          
+          # Show to screen
+          cv2.imshow('open_image',image)
+          ret, buffer = cv2.imencode('.jpg', image)
+          frame = buffer.tobytes()
+          yield (b'--frame\r\n'
+                 b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')        
+
+                           
+
+          # Break gracefully
+          if cv2.waitKey(10) & 0xFF == ord('q'):
+              break
+      cap.release()
+      cv2.destroyAllWindows()
+        
+def gen_frames():
+    cap = cv2.VideoCapture(0)
+	
+    	
+    while True:
+        print("1")
+        success, frame = cap.read()  # read the camera frame
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    socketio.run(app)
