ccb7da96fb98af0318d0d76f9df75c91b3811251
akashrajcashrayz <53985845+akashrajcashrayz@users.noreply.github.com>
2021-08-23 18:42:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -117,7 +117,7 @@ def gen():
 
           # Read feed
           #ret, frame = cap.read()
-          frame = Camera.get_frame()
+          frame = camera.get_frame()
           #frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)  
           # Make detections
           image, results = mediapipe_detection(frame, holistic)
