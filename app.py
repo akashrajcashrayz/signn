6abd4d2fb357e1b3ccd7904cfcca4848f463dceb
akashrajcashrayz <53985845+akashrajcashrayz@users.noreply.github.com>
2021-08-24 08:25:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -132,9 +132,11 @@ def gen():
           # Read feed
           #ret, frame = cap.read()
           frame = camera.get_frame()
-          frame = readb64(frame)
+          frame = binascii.b2a_base64(frame)
+          print(frame) 
+          frame = readb64(frame)  
           print(frame)
-          #frame = base64_to_pil_image(frame)  
+        
           #frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)  
           # Make detections
           image, results = mediapipe_detection(frame, holistic)
