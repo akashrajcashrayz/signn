e0477d918171b913776d63f584cd611b2375b3df
akashrajcashrayz <53985845+akashrajcashrayz@users.noreply.github.com>
2021-08-24 11:49:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -143,6 +143,8 @@ def gen():
           frame = readb64(frame)
           #frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
           print(frame.shape) 
+          ret, buffer = cv2.imencode('.jpg', frame)
+          frame = buffer.tobytes()            
           yield (b'--frame\r\n'
                  b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')              
           #print(frame)
